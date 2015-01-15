3aa802331c5ea723429e2da24b5455236a37c3b3
Gerwin van Royen <gvanroyen@gmail.com>
2015-01-16 07:45:43 +1300

    prevent form submit

--- demos/06_views_and_forms_and_helpers/views/main.js
+++ demos/06_views_and_forms_and_helpers/views/main.js
@@ -6,7 +6,7 @@ flour.addView('main', function(){
   view.template = 'main';
 
   view.events = {
-    'submit form': 'displayData'
+    'submit form': 'save'
   };
 
   view.helpers = ['form'];
@@ -27,5 +27,11 @@ flour.addView('main', function(){
       'description': 'Gandalf the Grey, later known as Gandalf the White, was a wizard, or Istar, sent by the West in the Third Age to combat the threat of Sauron. He joined Thorin and his company to reclaim Erebor from Smaug, convoked the Fellowship of the Ring to destroy the One Ring, and led the Free Peoples in the final campaign of the War of the Ring.'
     });
   };
+
+
+  // on form submit
+  view.save = function(event, el){
+    event.preventDefault();
+  }
 
 });
