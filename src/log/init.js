bdbcb93066e240a872bc46d59c012a0038f47982
Gerwin van Royen <gvanroyen@gmail.com>
2018-09-07 07:18:55 +1200

    log now displays window.onerror errors events

--- src/log/init.js
+++ src/log/init.js
@@ -26,6 +26,24 @@ flour.logView = flour.getView('flour_log');
 
 
 $(function(){ 
+
+	window.onerror = function (msg, url, lineNo, columnNo, error) 
+  {
+    var string = msg.toLowerCase();
+    var substring = "script error";
+    if(string.indexOf(substring) > -1)
+    {
+      flour.error('Script Error: See Browser Console for Detail');
+    } 
+    else
+    {
+      url = url.split('/').pop();
+      flour.error((msg + '<br />' +  url + ' (' + lineNo + ', ' + columnNo + ')'), JSON.stringify(error));
+    }
+
+    return false;
+  };
+
 	$('body').append(flour.logView.el); 
 });
 
